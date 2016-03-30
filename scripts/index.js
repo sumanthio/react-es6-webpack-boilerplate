8a0e0b8895ffb0b894c4ad32458972ae61b29453
sumanthio <sumanthp544@gmail.com>
2016-03-30 12:29:13 +0530

    React router added

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import {render} from 'react-dom';
 import App from './App';
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+//var appHistory = routerHistory(createHistory)({ queryKey: false });
 
 render(
-  <App />,
+  //<App />,
+  <Router history={browserHistory}>
+   <Route path="/" component={App}>
+     <Route path="about" component={App} />
+     <Route path="inbox" component={App}>
+       <Route path="messages/:id" component={App} />
+     </Route>
+   </Route>
+ </Router>,
   document.getElementById('root')
 );
